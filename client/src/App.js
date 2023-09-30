677a4e3a8bb8c1840a340eb5b880ad6d01822e91
Killian <killian.hedou@gmail.com>
2023-09-29 23:48:38 -0400

    Added better Mine & Diamond visuals. Money tracking added for bet and profit. Algorithm to increase profit for each additional mine slot revealed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
-import { createElement } from 'react';
 import './App.css';
 
 function App() {
+  //Global variables
   let hiddenCount = 16;
   let mineCount = 2;
+  let balance = 100;
+  let bet = 10;
+  let profit = 0;
+  let returnMoney = bet;
 
-  function mineLogic(e){
-    let image =  e.target.childNodes[1]
+  function mineLogic(e) {
+    let image =  e.target.childNodes[0];
     image.display = "block";
     e.target.disabled = true;
-    let randomNumber = Math.random(0) * hiddenCount;
+    let randomNumber = Math.floor(Math.random() * hiddenCount);
     if (randomNumber > mineCount){
       hiddenCount--;
       console.log(hiddenCount)
-      image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJOQoEJ96e8C2yYRnBkIDL0JXM7byzdChafi0MTeTO&s"
+      image.src = "https://images.vexels.com/media/users/3/157265/isolated/preview/d546c542730b45e5893fc0ed71c8f4d7-blue-diamond-stone-vector.png";
+      //adjust bet and round to 2 decimal places
+      returnMoney *= (1 + mineCount/hiddenCount);
+      profit = returnMoney - bet;
+      document.getElementById("betInfo").innerHTML = "Balance: $" + balance + "<br/>Bet: $" + bet + "<br/>Profit: $" + profit.toFixed(2);
+      document.getElementById("cashoutBtn").innerHTML = "Cashout: $" + returnMoney.toFixed(2);
     }
     else {
-      image.src = "https://img.freepik.com/premium-vector/cannon-balls-cute-cartoon-black-bomb_634248-10.jpg"
-      console.log("hit mine")
+      image.src = "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/60401/bomb-clipart-xl.png";
+      console.log("hit mine");
+      returnMoney = 0;
+      profit = returnMoney - bet;
+      document.getElementById("betInfo").innerHTML = "Balance: $" + balance + "<br/>Bet: $" + bet + "<br/>Profit: $" + profit.toFixed(2);
+      document.getElementById("cashoutBtn").innerHTML = "Cashout: $" + returnMoney.toFixed(2);
     }
     // hit gem -> hiddenCount--;
     
@@ -27,8 +40,7 @@ function App() {
     const mines = Array.from({ length: 16 }).map((_, index) => (
       <div key={index} className="mine">
         <button className='mineBtn' onClick={mineLogic}>
-          M
-          <img display="none" src=""></img>
+          <img className="mineImg" display="none" src="" alt=''/>
         </button>
       </div>
     ));
@@ -37,13 +49,13 @@ function App() {
   return (
     <div className="App">
       <button className="btn btn-primary login">Log in</button>
-
-
-      <div className="mineContainer">
-      {mines}
-
+      <div className='mineGame'>
+        <div className="mineContainer">
+        {mines}
+        </div>
+        <div id="betInfo" className="betInfo">Balance: ${balance} <br/>Bet: ${bet}<br/>Profit: $0</div>
+        <button id='cashoutBtn' className="btn btn-success cashoutBtn">Cashout: ${returnMoney}</button>
       </div>
-
     </div>
   );
 }
