be79c96668dd359d29fea285574180525dd7234f
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2023-09-29 22:48:26 -0400

    added images and mines logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
+import { createElement } from 'react';
 import './App.css';
 
 function App() {
+  let hiddenCount = 16;
+  let mineCount = 2;
 
-  
+  function mineLogic(e){
+    let image =  e.target.childNodes[1]
+    image.display = "block";
+    e.target.disabled = true;
+    let randomNumber = Math.random(0) * hiddenCount;
+    if (randomNumber > mineCount){
+      hiddenCount--;
+      console.log(hiddenCount)
+      image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJOQoEJ96e8C2yYRnBkIDL0JXM7byzdChafi0MTeTO&s"
+    }
+    else {
+      image.src = "https://img.freepik.com/premium-vector/cannon-balls-cute-cartoon-black-bomb_634248-10.jpg"
+      console.log("hit mine")
+    }
+    // hit gem -> hiddenCount--;
+    
+  }
 
     // Generate a 4x4 grid of div elements
     const mines = Array.from({ length: 16 }).map((_, index) => (
       <div key={index} className="mine">
-        <button className='mineBtn'>M</button>
+        <button className='mineBtn' onClick={mineLogic}>
+          M
+          <img display="none" src=""></img>
+        </button>
       </div>
     ));
 
