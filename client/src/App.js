fac1aef21afcad2d3ebb7b48045b68bd882ddb5d
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2023-09-30 03:01:46 -0400

    fully implemented cashout

--- client/src/App.js
+++ client/src/App.js
@@ -16,36 +16,37 @@ function App() {
   }
 
   function mineLogic(e) {
+    if (!document.getElementById("betBtn").disabled)
+      handleBet();
+
     document.getElementById("betBtn").disabled = true;
     document.getElementById("betInput").disabled = true;
     document.getElementById("cashoutBtn").disabled = false;
+    document.getElementById("cashoutBtn").onclick = handleCashout;
     let image =  e.target.childNodes[0];
     image.display = "block";
     e.target.disabled = true;
     let randomNumber = Math.floor(Math.random() * hiddenCount);
-    if (randomNumber > mineCount){
+    if (randomNumber => mineCount){
       hiddenCount--;
-      console.log(hiddenCount)
       image.src = "https://images.vexels.com/media/users/3/157265/isolated/preview/d546c542730b45e5893fc0ed71c8f4d7-blue-diamond-stone-vector.png";
       //adjust bet and round to 2 decimal places
       returnMoney *= (1 + mineCount/hiddenCount);
-      console.log(returnMoney)
       profit = returnMoney - bet;
       updateLabels()
     }
     else {
       image.src = "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/60401/bomb-clipart-xl.png";
-      console.log("hit mine");
       returnMoney = 0;
       profit = returnMoney - bet;
       updateLabels()
-    }
-    // hit gem -> hiddenCount--;
-    
+    }   
   }
   
   function handleBetInput(e){
-    if (e.target.value > balance){
+    let betValue = parseFloat(e.target.value)
+    if (betValue == "" || isNaN(betValue) || betValue > balance || betValue < 0){
+      document.getElementById("cashoutBtn").disabled = true;
       document.getElementById("betBtn").disabled = true;
       let mineBtns = document.getElementsByClassName("mineBtn");
       for (let i = 0; i < mineBtns.length; i++){
@@ -98,7 +99,6 @@ function App() {
     updateLabels();
     resetGame();
   }
-  
     // Generate a 4x4 grid of div elements
     const mines = Array.from({ length: 16 }).map((_, index) => (
       <div key={index} className="mine">
@@ -122,7 +122,7 @@ function App() {
         {mines}
         </div>
         <div id="betInfo" className="betInfo">Balance: ${balance} <br/>Bet: ${bet}<br/>Profit: $0</div>
-        <button id='cashoutBtn' className="btn btn-success cashoutBtn" onClick={handleCashout}>Cashout: ${returnMoney}</button>
+        <button id='cashoutBtn' className="btn btn-success cashoutBtn" onClick={handleCashout} disabled={true}>Cashout: ${returnMoney}</button>
       </div>
     </div>
   );
